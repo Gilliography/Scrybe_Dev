9124feadc58f06c0d54e9a3ff8a1ac7ca2574236
Gilbert <gkiprotich74@gmail.com>
2023-08-09 18:03:16 +0300

    Conditions in Python

--- Python/conditions.py
+++ Python/conditions.py
@@ -1,8 +1,8 @@
-n= input("Number:")
+n= int (input("Number:"))
 if n>0:
     print("n is greater than positive")
 elif n<0:
     print("n is negative")
 else:
-    print("ne i zero")
+    print("n is zero")
     
