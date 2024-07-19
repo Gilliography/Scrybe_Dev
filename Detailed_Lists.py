97862ca48d4a26a06922b203e48f9aa66fbd9cc9
Gilbert <gkiprotich74@gmail.com>
2024-07-19 10:36:52 +0300

    first commit

---
+++ Detailed_Lists.py
@@ -0,0 +1,20 @@
+names=["James", "John", "Allan", "Orenge"]
+print("Original list is:", names)
+
+#Using Append method to add elements to a list
+#append function takes only one argument as seen below
+names.append("Michuki")
+print("Updated list is:", names)
+
+#Using insert() function to add elements into a list
+names.insert(5, "Mwakazi")
+print("The list with newly inserted name is", names)
+
+#Printing a name in a specific index
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[3])
+
+
