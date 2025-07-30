8fe9791a90697cdea941a6ee17e9db5f9dee5c7c
Gilbert <gkiprotich74@gmail.com>
2025-07-30 11:36:19 +0300

    Grade Manager App

---
+++ Grade Manager.py
@@ -0,0 +1,31 @@
+class GradeManager:
+    def __init__(self, name, marks, grade):
+        self.name = name 
+        self.marks = marks
+        self.grade = grade
+
+        print(f"Grade Manager initialized for {self.name} with marks {self.marks} and grade {self.grade}.")
+
+name = input("Enter your name:")
+marks = int(input("Enter your marks:"))
+while True:
+    if marks > 100 or marks < 0:
+        print("Marks should be between 0 and 100. Please try again.")
+    elif marks >=0 and marks <=20:
+        grade = "F"
+    elif marks > 20 and marks <= 40:
+        grade = "D"
+    elif marks > 40 and marks <= 60:
+        grade = "C"
+    elif marks > 60 and marks <= 80:
+        grade = "B"
+    elif marks > 80 and marks <= 90:
+        grade = "A"
+    elif marks > 90 and marks <= 100:
+        grade = "A+"
+    else:
+        grade = "Invalid marks"
+    break
+
+grade_manager = GradeManager(name, marks, grade)
+print(f"Hi {name}, your grade is: {grade}")
