192eced78bac0efddc9b55fc6d0fb0b2c25eb5d9
Gilbert <gkiprotich74@gmail.com>
2025-07-30 11:41:14 +0300

    Addition of menu on  Manager App

--- Grade Manager.py
+++ Grade Manager.py
@@ -26,6 +26,24 @@ while True:
     else:
         grade = "Invalid marks"
     break
+def main():
+    while True:
+        try:
+            print("\n===Grade Manager Menu===")
+            print("1. Enter Student Details")
+            print("2. Enter Marks")
+            print("3. Exit")
+
+            choice = input("Enter your choice")
+
+            if choice == '1':
+                name = input("Enter student's name: ")
+                print(f"student's name is {name}.")
+            elif choice == '2':
+                marks = int(input("Enter student's marks: "))
+                print(f"student's marks are {marks}.")
+            elif choice == '3':
+                print("Exiting Grade Manager. Goodbye!")
 
 grade_manager = GradeManager(name, marks, grade)
 print(f"Hi {name}, your grade is: {grade}")
